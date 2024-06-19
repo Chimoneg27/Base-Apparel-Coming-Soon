e199577e63eb3b8cd28db5ac2d13512cc695b966
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-19 23:30:55 +0200

    Add some validation code

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let emailEl = document.getElementById('email');
 let submitBtn = document.getElementById('submit')
+let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+const errorMsg = document.getElementById('valid');
 
 submitBtn.addEventListener('click', function() {
     let emailValue = emailEl.value;
-    console.log(emailValue)
+    if(emailValue.match(regex)) {
+        console.log('Valid email')
+        errorMsg.innerHTML = ''
+    } else {
+        console.log('Invalid email')
+        errorMsg.innerHTML = 'Please enter a valid email address.'
+    }
+    emailEl.value = ''
 })
