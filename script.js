c06655a0a54ba147d5a89687410cbb1b038e6ce4
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-20 14:58:03 +0200

    add strict validation

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let emailEl = document.getElementById('email');
 let submitBtn = document.getElementById('submit')
-let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;;
 const errorMsg = document.getElementById('valid');
+const errorIcon = document.getElementById('error-icn')
 
 submitBtn.addEventListener('click', function() {
     let emailValue = emailEl.value;
-    if(emailValue.match(regex)) {
-        console.log('Valid email')
-        errorMsg.innerHTML = ''
-    } else {
-        console.log('Invalid email')
-        errorMsg.innerHTML = 'Please enter a valid email address.'
-    }
-    emailEl.value = ''
+    validateEmail(emailValue)
 })
+
+const matchRegex = (email) => {
+    return email.match(regex)
+}
+
+const validateEmail = (emailVal) => {
+    if(matchRegex(emailVal)) {
+        errorMsg.innerHTML = ''
+        errorIcon.style.display = 'none'
+        emailEl.value = ''
+    } else {
+        errorMsg.innerHTML = 'Please enter a valid email address.'
+        errorIcon.style.display = 'block'
+    }
+}
